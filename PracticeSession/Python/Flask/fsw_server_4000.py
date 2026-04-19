820b8a5b5af4e3cfd86c7f02c3c260a5fc87762f
brainstorm8 <brainstorm8mueen@gmail.com>
2026-04-20 02:14:08 +0530

    update_200426_3

--- PracticeSession/Python/Flask/fsw_server_4000.py
+++ PracticeSession/Python/Flask/fsw_server_4000.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify , render_template
 
-app = Flask(__name__)
-#app = Flask(__name__, template_folder='fullstackweb/templates')
+#app = Flask(__name__)
+app = Flask(__name__, template_folder='fullstackweb/templates')
 
 @app.route('/')
 def home_page():
