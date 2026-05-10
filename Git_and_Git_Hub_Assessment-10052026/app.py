285c52a10f5075568caa9772f0f57b91c58a7824
brainstorm8 <brainstorm8mueen@gmail.com>
2026-05-10 23:15:45 +0530

    Add greet function and enhance app output

--- Git_and_Git_Hub_Assessment-10052026/app.py
+++ Git_and_Git_Hub_Assessment-10052026/app.py
@@ -1,5 +1,9 @@
+def greet(name):
+    return f"Hello, {name}!"
+
 def main():
-    print("Hello from VS Code Terminal!")
+    print("Hello, Git World! from VS Code Terminal!")
+    print(greet("Mueen"))   
 
 if __name__ == "__main__":
     main()
