75a36b15635d042e67965c36eb4a838098cd1f8d
brainstorm8 <brainstorm8mueen@gmail.com>
2026-05-10 23:18:15 +0530

    Update greeting message

--- Git_and_Git_Hub_Assessment-10052026/app.py
+++ Git_and_Git_Hub_Assessment-10052026/app.py
@@ -1,9 +1,9 @@
 def greet(name):
-    return f"Hello, {name}!"
+    return f"Welcome, {name}!"
 
 def main():
     print("Hello, Git World! from VS Code Terminal!")
-    print(greet("Mueen"))   
+    print(greet("Mueen")) 
 
 if __name__ == "__main__":
     main()
