5bc370863fcb66249bb325de69bed3e7e56788b9
brainstorm8 <brainstorm8mueen@gmail.com>
2026-05-10 23:56:43 +0530

    images

--- Git_and_Git_Hub_Assessment-10052026/app.py
+++ Git_and_Git_Hub_Assessment-10052026/app.py
@@ -1,14 +1,9 @@
-def calculate_sum(a, b):
-    return a + b
-
 def greet(name):
     return f"Welcome, {name}!"
 
 def main():
     print("Hello, Git World! from VS Code Terminal!")
-    print(greet("Mueen"))
-    result = calculate_sum(5, 3)
-    print(f"Sum is: {result}")
+    print(greet("Mueen")) 
 
 if __name__ == "__main__":
     main()
