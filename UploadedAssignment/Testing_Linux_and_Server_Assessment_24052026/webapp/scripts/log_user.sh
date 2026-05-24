bc28561f5efa57667de055fdda81ab73a9132f5d
brainstorm8 <brainstorm8mueen@gmail.com>
2026-05-25 02:29:56 +0530

    Re-add folder after deletion

--- UploadedAssignment/Testing_Linux_and_Server_Assessment_24052026/webapp/scripts/log_user.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-read -p "Enter your name: " username
-cat /home/mueenb/webapp/config/app.conf
-echo "Login: $username Date: $(date)" >> /home/mueenb/webapp/logs/app.log
-cat /home/mueenb/webapp/logs/app.log
-test by devuser1
-test by devuser2
